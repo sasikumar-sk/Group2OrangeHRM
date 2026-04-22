3b309346fab0eb68a01c7784cb8f5e9efa2f2fea
Pooja BK <DK1145@desicrew.in>
2026-04-22 13:49:36 +0530

    Add Verify search employee automation scripts

---
+++ pages/login.js
@@ -0,0 +1,37 @@
+export class LoginPage {
+
+  constructor(page) {
+
+    this.page = page
+    // Login fields
+
+    this.username = page.getByPlaceholder('Username')
+    this.password = page.getByPlaceholder('Password')
+    this.loginButton = page.getByRole('button', { name: 'Login' })
+
+    // invalid credentials error message
+    this.errorMsg = page.getByText('Invalid credentials')
+
+    //Dashboard elements for Admin
+    this.dashboard = page.getByRole('heading', { name: 'Dashboard' })
+
+    // Menu items for Admin
+    this.adminMenu = page.getByRole('link', { name: 'Admin' })
+    this.pimMenu = page.getByRole('link', { name: 'PIM' })
+    this.leaveMenu = page.getByRole('link', { name: 'Leave' })
+    this.myInfoMenu = page.getByRole('link', { name: 'My Info' })
+  }
+
+  async goto() {
+    await this.page.goto('/web/index.php/auth/login')
+  }
+
+  async login(username, password) {
+    await this.username.fill(username)
+    await this.password.fill(password)
+    await this.loginButton.click()
+  }
+
+}
+
+module.exports = { LoginPage }
--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+
+      baseURL: 'https://opensource-demo.orangehrmlive.com',
     trace: 'on-first-retry',
   },
 
